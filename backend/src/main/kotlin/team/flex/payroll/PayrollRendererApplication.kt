056f8822856ed854e534e8d8fc9a20122d251696
김주원 <143988486+flex-juwon@users.noreply.github.com>
2025-07-25 15:38:50 +0900

    :construction_worker: 빌드 스크립트를 개선한다

--- backend/src/main/kotlin/team/flex/payroll/PayrollRendererApplication.kt
+++ backend/src/main/kotlin/team/flex/payroll/PayrollRendererApplication.kt
@@ -2,10 +2,56 @@ package team.flex.payroll
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.InputStream
 
 @SpringBootApplication
 class PayrollRendererApplication
 
 fun main(args: Array<String>) {
     runApplication<PayrollRendererApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/hello")
+class HelloController(
+    private val pdfRenderer: PdfRenderer
+) {
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_PDF_VALUE])
+    fun hello(@RequestBody command: HelloCommand): ResponseEntity<InputStreamResource> {
+        val pdfBytes = pdfRenderer.render(command)
+        val content = InputStreamResource(ByteArrayInputStream(pdfBytes))
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE)
+            .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=hello.pdf")
+            .body(content)
+    }
+}
+
+data class HelloCommand(val name: String)
+
+@Service
+class PdfRenderer {
+    fun render(command: HelloCommand): ByteArray {
+        val process = ProcessBuilder("ls", "-al")
+            .start()
+
+        return process.inputStream.use(InputStream::readAllBytes).also {
+            val exitCode = process.waitFor()
+            if (exitCode != 0) {
+                throw RuntimeException("Process exited with non-zero exit code: $exitCode")
+            }
+        }
+    }
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,8 +6,12 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('app') as HTMLElement
 );
+
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get('name') || 'Anonymous';
+
 root.render(
   <React.StrictMode>
-    <App name={'john'}/>
+    <App name={name}/>
   </React.StrictMode>
 );
